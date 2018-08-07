7ef47014b3b2802ee335c82e4f05adfcde4432f8
MarynaKontar <marina.acoustic@gmail.com>
2018-08-07 08:28:52 +0300

    have error for gh-pages. delete BrowserAnimationsModule from testing.module

--- src/app/testing/testing.module.ts
+++ src/app/testing/testing.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ValueCompatibilityComponent } from './value-compatibility/value-compatibility.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    BrowserAnimationsModule
   ],
   declarations: [ValueCompatibilityComponent]
 })
