d9e072a183d703d5a7634936d2ebb9a680bb56b1
MarynaKontar <marina.acoustic@gmail.com>
2018-08-21 16:46:34 +0300

    add animation for value-compatibility test cards - slide. Data isn't save to database - instead of console.log. It's for testing frontend without backend

--- src/animations/testing-page-animation.ts
+++ src/animations/testing-page-animation.ts
@@ -1,8 +1,31 @@
-import {animate, state, style, transition, trigger} from '@angular/animations';
+import {animate, keyframes, state, style, transition, trigger} from '@angular/animations';
 
-export let test = trigger('test', [
+export let fade = trigger('fade', [
   state('void', style({ opacity: 0 })), // полностью прозрачен в состоянии void
   transition(':enter, :leave', [ // void <=> *
-    animate('3s ease-in-out')
+    animate('2s ease-in-out')
   ])
+]);
+
+export let slide = trigger('slide', [
+  state('active', style({ transform: 'translateX(0%)'})),
+  state('unactive',  style({ transform: 'translateX(300%)'})),
+  // state('unactive',  style({ backgroundColor: '#f00', transform: 'translateX(300%)'})),
+  transition('active => unactive',
+    // animate(1000, style({ transform: 'translateX(-200%)'}))
+    animate('.5s ease-in', keyframes([
+      // !!!!!! Добавить в функции setGoal(i: number, scale: Scale) паузу в 1 секунду (setTimeout (() => ..., 1000);)
+      style({opacity: 1, transform: 'translateX(0%)', offset: 0}),
+      style({opacity: .5, transform: 'translateX(-35px)', offset: .3}),
+      style({opacity: 0, transform: 'translateX(-200%)', offset: 1})
+    ]))
+  ),
+  transition('unactive => active',
+    // animate(1000, style({ transform: 'translateX(0%) translateY(-0%)'}))
+    animate('.5s ease-in', keyframes([
+      style({opacity: 0, transform: 'translateX(200%)', offset: 0}),
+      style({opacity: .5, transform: 'translateX(35px)', offset: .3}),
+      style({opacity: 1, transform: 'translateX(0%)', offset: 1}),
+    ]))
+  )
 ]);
--- src/app/testing/value-compatibility/value-compatibility.component.ts
+++ src/app/testing/value-compatibility/value-compatibility.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ValueCompatibilityService} from '../value-compatibility.service';
 import {Scale, tests, ValueCompatibilityAnswers} from './value-compatibility-answers';
 import {FormArray, FormBuilder, FormControl, FormGroup} from '@angular/forms';
-import {Observable} from 'rxjs';
-import {User} from '../../profile/user';
-import {test} from '../../../animations/testing-page-animation';
+import {slide, fade} from '../../../animations/testing-page-animation';
+import {Router} from '@angular/router';
 
 
 @Component({
@@ -12,7 +11,7 @@ import {test} from '../../../animations/testing-page-animation';
   templateUrl: './value-compatibility.component.html',
   styleUrls: ['./value-compatibility.component.scss'],
   animations: [
-    test
+    fade, slide
   ]
 })
 export class ValueCompatibilityComponent implements OnInit {
@@ -25,11 +24,20 @@ export class ValueCompatibilityComponent implements OnInit {
   //   { id: 3, name: 'order 3' },
   //   { id: 4, name: 'order 4' }
   // ];
+  isGoalsDone = false;
+  isStatesDone = false;
+  isQualitiesDone = false;
   answer = [];
+  ind = 0;
+  isNotPassed = true;
+  itemState = [];
+  animationTime = 500;
+
   // data: Observable<ValueCompatibilityAnswers>;
 
   constructor(private valueCompatibilityService: ValueCompatibilityService,
-              private formBuilder: FormBuilder) {
+              private formBuilder: FormBuilder,
+              private router: Router) {
     // Create a new array with a form control for each order
     // const controls = this.orders.map(c => new FormControl(false));
     // controls[0].setValue(true); // Set the first checkbox to true (checked)
@@ -47,6 +55,22 @@ export class ValueCompatibilityComponent implements OnInit {
     //     }
     //   );
     this.tests = tests;
+    console.log(this.tests);
+    this.itemState[0] = 'active';
+    this.itemState[1] = 'unactive';
+    this.itemState[2] = 'unactive';
+    this.itemState[3] = 'unactive';
+    this.itemState[4] = 'unactive';
+    this.itemState[5] = 'unactive';
+    this.itemState[6] = 'unactive';
+    this.itemState[7] = 'unactive';
+    this.itemState[8] = 'unactive';
+    this.itemState[9] = 'unactive';
+    this.itemState[10] = 'unactive';
+    this.itemState[11] = 'unactive';
+    this.itemState[12] = 'unactive';
+    this.itemState[13] = 'unactive';
+    this.itemState[14] = 'unactive';
   }
 
 
@@ -70,16 +94,58 @@ export class ValueCompatibilityComponent implements OnInit {
   //   console.log(this.answer);
   // }
 
+  isActive(i: number) {
+   return i === this.ind;
+  }
+  isChosen(i: number) {
+    return this.tests.goal[i].chosenScale !== null;
+  }
   setGoal(i: number, scale: Scale) {
+    this.itemState[i] = 'unactive';
     this.tests.goal[i].chosenScale = scale;
+
+    // нужна пауза, чтобы успела пройти анимация 'active => unactive' (пауза должна быть такой же как в анимации "slide")
+    setTimeout (() => {
+      this.ind = i + 1;
+      this.itemState[i + 1] = 'active';
+      // Добавить проверку на Passed
+      if (this.ind === 15) {
+        this.isNotPassed = false;
+      }
+      // this.router.navigate(['register']);
+    }, this.animationTime);
   }
 
   setState(i: number, scale: Scale) {
+    this.itemState[i] = 'unactive';
     this.tests.state[i].chosenScale = scale;
+
+    // нужна пауза, чтобы успела пройти анимация 'active => unactive' (пауза должна быть такой же как в анимации "slide")
+    setTimeout (() => {
+      this.ind = i + 1;
+      this.itemState[i + 1] = 'active';
+      // Добавить проверку на Passed
+      if (this.ind === 15) {
+        this.isNotPassed = false;
+      }
+      // this.router.navigate(['register']);
+    }, this.animationTime);
   }
 
   setQuality(i: number, scale: Scale) {
+    this.itemState[i] = 'unactive';
     this.tests.quality[i].chosenScale = scale;
+
+    // нужна пауза, чтобы успела пройти анимация 'active => unactive' (пауза должна быть такой же как в анимации "slide")
+    setTimeout (() => {
+      this.ind = i + 1;
+      this.itemState[i + 1] = 'active';
+      // Добавить проверку на Passed
+      if (this.ind === 15) {
+        this.isNotPassed = false;
+      }
+      // this.router.navigate(['register']);
+    }, this.animationTime);
   }
 
   // saveGoals(tests: ValueCompatibilityAnswers): void {
@@ -88,14 +154,49 @@ export class ValueCompatibilityComponent implements OnInit {
     // console.log(data));
     // console.log(this.tests.goal);
       console.log(this.tests);
+      this.isGoalsDone = true;
+    this.itemState[0] = 'active';
+    this.ind = 0;
+    this.isNotPassed = true;
+    this.router.navigate(['user-test']);
   }
-
+  resetGoals() {
+    // this.valueCompatibilityService.getTestList()
+    //   .subscribe(data => {
+    //       console.log(data);
+    //       this.tests.goal = data.goal;
+    //     }
+    //   );
+    this.itemState[0] = 'active';
+    this.tests.goal = tests.goal;
+    this.ind = 0;
+    this.isNotPassed = true;
+    this.router.navigate(['user-test']);
+  }
 
   saveStates() {
     // this.valueCompatibilityService.saveStateArray(this.tests.state).subscribe(data =>
       // console.log(data));
     // console.log(this.tests.state);
     console.log(this.tests);
+    this.isStatesDone = true;
+    this.itemState[0] = 'active';
+    this.ind = 0;
+    this.isNotPassed = true;
+    this.router.navigate(['user-test']);
+  }
+  resetStates() {
+    // this.valueCompatibilityService.getTestList()
+    //   .subscribe(data => {
+    //       console.log(data);
+    //       this.tests.state = data.state;
+    //     }
+    //   );
+    this.itemState[0] = 'active';
+    this.tests.state = tests.state;
+    this.ind = 0;
+    this.isNotPassed = true;
+    this.router.navigate(['user-test']);
   }
 
   saveQualities() {
@@ -103,6 +204,24 @@ export class ValueCompatibilityComponent implements OnInit {
       // console.log(data));
     // console.log(this.tests.quality);
       console.log(this.tests);
+      this.isQualitiesDone = true;
+    this.itemState[0] = 'active';
+    this.ind = 0;
+    this.isNotPassed = true;
+    this.router.navigate(['user-test']);
+  }
+  resetQualities() {
+    // this.valueCompatibilityService.getTestList()
+    //   .subscribe(data => {
+    //       console.log(data);
+    //       this.tests.quality = data.quality;
+    //     }
+    //   );
+    this.itemState[0] = 'active';
+    this.tests.quality = tests.quality;
+    this.ind = 0;
+    this.isNotPassed = true;
+    this.router.navigate(['user-test']);
   }
 
   isFirstTestItem(i): boolean {
